55202d60d637011da063858ca534b2637cad3de3
suchi.cheena@gmail.com <suchi.cheena@gmail.com>
2019-01-16 22:31:13 +0530

    Initial commit

---
+++ src/main/java/com/suchi/fuber/demo_fuber/DemoFuberApplication.java
@@ -0,0 +1,14 @@
+package com.suchi.fuber.demo_fuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFuberApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/suchi/fuber/demo_fuber/DemoFuberApplicationTests.java
@@ -0,0 +1,17 @@
+package com.suchi.fuber.demo_fuber;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoFuberApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
